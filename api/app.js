711f132ece8d60b3441804fc0845d5ec34584de3
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-10-25 23:27:46 +0300

    refactor: move all files to api folder

--- api/app.js
+++ api/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
 
